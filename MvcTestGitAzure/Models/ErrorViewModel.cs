edb50b8f57792a538edfca7e9e9f637c6435bd8b
Kaushik <kaushikb2005@yahoo.co.in>
2020-07-21 13:11:03 -0500

    Initial check in

---
+++ MvcTestGitAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcTestGitAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
